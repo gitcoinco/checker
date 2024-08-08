3c6be67896b65c049fe35a04aa9e68fe3d07dc7e
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-08-08 21:04:44 +0530

    Fix api url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,13 @@
 import { initQueryClient } from "@ts-rest/react-query";
 import { contract } from "~/routes/contract";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 export const api = initQueryClient(contract, {
-  baseUrl: "http://localhost:3000/api/",
+  baseUrl: `${getBaseUrl()}/api/`,
   baseHeaders: {},
 });
