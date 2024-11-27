1d18a6a11c13aec75b37632a43d3188d3a7c9538
0xKurt <Kurt@Merbeth.io>
2024-11-27 19:41:34 +0100

    fix checker logo import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "./providers/Providers";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import { CheckerIcon } from "gitcoin-ui";
 
 import dynamic from "next/dynamic";
 
