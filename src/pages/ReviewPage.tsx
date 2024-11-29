102c4769f3acf83832d70cff7f929ccfbcc59c67
Nick Lionis <nikolaos@gitcoin.co>
2024-11-29 09:51:15 +0700

    integrates hooks in ReviewPage + logic to switch to correct chain

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,33 +1,54 @@
 import { Checker } from "gitcoin-ui/checker";
+import { usePerformOnChainReview, usePerformEvaluation } from "@/hooks";
+import { Button } from "gitcoin-ui";
 import { useParams } from "react-router";
-import { useAccount } from "wagmi";
+import { useAccount, useWalletClient } from "wagmi";
 
 export const ReviewPage = () => {
   const { chainId, poolId } = useParams();
+  const { address, chainId: connectedChainId } = useAccount();
+  const { data: walletClient } = useWalletClient();
+
+  const { setEvaluationBody, isSigning, isSuccess, isEvaluating, isError, isErrorSigning } =
+    usePerformEvaluation();
+  const { steps, setReviewBody, isReviewing } = usePerformOnChainReview();
 
   if (!chainId || !poolId) {
     return <div>Invalid pool ID</div>;
   }
-  const { address } = useAccount();
-
-  if (!address) {
+  if (!address || !walletClient) {
     return <div>Connect your wallet</div>;
+  }
+
+  if (Number(chainId) !== connectedChainId) {
+    return (
+      <div className="mt-[30%] flex flex-col items-center">
+        <Button
+          onClick={async () => {
+            await walletClient.switchChain({
+              id: Number(chainId),
+            });
+          }}
+          value="Switch chain"
+        />
+      </div>
+    );
   }
 
   return (
     <Checker
       address={address}
-      chainId={parseInt(chainId, 10)}
       poolId={poolId}
-      setEvaluationBody={() => {}}
-      isSigning={false}
-      isSuccess={false}
-      isEvaluating={false}
-      isError={false}
-      isErrorSigning={false}
-      steps={[]}
-      setReviewBody={() => {}}
-      isReviewing={false}
+      chainId={parseInt(chainId, 10)}
+      setEvaluationBody={setEvaluationBody}
+      isSigning={isSigning}
+      isSuccess={isSuccess}
+      isEvaluating={isEvaluating}
+      isError={isError}
+      isErrorSigning={isErrorSigning}
+      steps={steps}
+      setReviewBody={setReviewBody as any}
+      isReviewing={isReviewing}
     />
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
