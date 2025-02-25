cd2f6d0f082b95642a7c04a5888eabc02ad19577
nijoe1 <nikolaos@gitcoin.co>
2025-02-25 15:18:44 -0700

    chore: refactor wagmi config

---
+++ src/providers/wagmiConnectors.ts
@@ -0,0 +1,35 @@
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  coinbaseWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  safeWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
+const wallets = [
+  metaMaskWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  coinbaseWallet,
+  rainbowWallet,
+  safeWallet,
+];
+
+/**
+ * wagmi connectors for the wagmi context
+ */
+export const wagmiConnectors = connectorsForWallets(
+  [
+    {
+      groupName: "Supported Wallets",
+      wallets,
+    },
+  ],
+
+  {
+    appName: "Gitcoin: Checker app",
+    projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID ?? "42a4d689485d5a324116e53ed077d907",
+  },
+);
