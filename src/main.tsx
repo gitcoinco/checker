f0b9dffdb86d6fb16b7f4dcee59747b630ef2000
Jedi Maker <meremezeukah@gmail.com>
2025-06-18 15:17:31 +0300

    setup CheckerProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,22 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import { LandingPage, ReviewPage, ViewPage } from "@/pages";
 import { MainLayout } from "@/layouts";
+import { CheckerProvider } from "@gitcoin/ui/checker";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="review/:chainId/:poolId" element={<ReviewPage />} />
-          <Route path="view/application/:chainId/:poolId/:applicationId" element={<ViewPage />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-      </Routes>
-    </BrowserRouter>
+    <CheckerProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="review/:chainId/:poolId" element={<ReviewPage />} />
+            <Route path="view/application/:chainId/:poolId/:applicationId" element={<ViewPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </BrowserRouter>
+    </CheckerProvider>
   </StrictMode>,
 );
